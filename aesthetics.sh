589ed6f22384d96ef43f214300f3158a1d3299bf
roufsyed <afrid704@gmail.com>
2021-09-28 14:05:06 +0530

    Added aesthetics.sh

---
+++ aesthetics.sh
@@ -0,0 +1,30 @@
+gsettings set org.gnome.shell.extensions.dash-to-dock background-color '#ffffff'
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock apply-glossy-effect true
+gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.2
+# gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode Fixed
+gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme false
+
+#Alphas
+gsettings set org.gnome.shell.extensions.dash-to-dock customize-alphas true
+gsettings set org.gnome.shell.extensions.dash-to-dock min-alpha 0.2
+gsettings set org.gnome.shell.extensions.dash-to-dock max-alpha 0.8
+
+gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.2
+gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide true
+gsettings set org.gnome.shell.extensions.dash-to-dock icon-size-fixed true
+gsettings set org.gnome.shell.extensions.dash-to-dock hide-delay 0.2
+gsettings set org.gnome.shell.extensions.dash-to-dock height-fraction 0.9
+gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner false
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
+gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner false
+
+#Running Dot color theme
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots true
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-color '#ffffff'
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-width 0
+gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color '#ffffff'
