62f6d88806d860a3dfdac164f5b4f3fc4b9bc1ef
roufsyed <afrid704@gmail.com>
2021-09-28 16:21:44 +0530

    Added ../ to go back a dir

--- nvimRemove.sh
+++ nvimRemove.sh
@@ -12,4 +12,4 @@ sudo apt remove --purge nodejs -y
 sudo apt remove --purge npm -y
 sudo apt-get remove --purge fuse libfuse2 git python3-pip ack-grep -y
 curl -sL remove-node.now.sh/lts | bash 
-rm -rf .config/nvim
+rm -rf ../.config/nvim
