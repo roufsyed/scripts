4009e45b12cb4174487c1f38b13ed975b8d0140f
roufsyed <afrid704@gmail.com>
2021-09-30 07:08:37 +0530

    Minor refactoring

--- oldsnapclean.sh
+++ oldsnapclean.sh
@@ -2,7 +2,7 @@
 # Removes old revisions of snaps
 # CLOSE ALL SNAPS BEFORE RUNNING THIS
 set -eu
-snap list --all | awk '/disabled/{print $1, $3}' |
+sudo snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
         snap remove "$snapname" --revision="$revision"
     done
