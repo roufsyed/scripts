3e9741ad5efd3f7342fca62dd020ae1359c7893c
roufsyed <afrid704@gmail.com>
2021-10-03 14:08:48 +0530

    Added desktop background configuration.
    1. Added curl command to downlaod wallpaper at usr/share/background/
    2. Added commands to set wallpaper and wallpaper zoom.

--- aesthetics.sh
+++ aesthetics.sh
@@ -1,3 +1,4 @@
+# Dock Configuraions 
 gsettings set org.gnome.shell.extensions.dash-to-dock background-color '#ffffff'
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
 gsettings set org.gnome.shell.extensions.dash-to-dock apply-glossy-effect true
@@ -28,3 +29,8 @@ gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-run
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-color '#ffffff'
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-width 0
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color '#ffffff'
+
+# Desktop background 
+sudo curl -o /usr/share/backgrounds/catalina.jpg --create-dirs https://4kwallpapers.com/images/wallpapers/macos-catalina-mountains-island-evening-stock-5k-1366x768-4021.jpg
+gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/catalina.jpg'
+gsettings set org.gnome.desktop.background picture-options 'zoom'
