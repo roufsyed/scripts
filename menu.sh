1d314986127e3c049355db984ce1ee6a84402db2
roufsyed <afrid704@gmail.com>
2021-11-03 16:09:03 +0530

    Made some menu changes to make more sense.

--- menu.sh
+++ menu.sh
@@ -1,10 +1,16 @@
+echo ''
+echo '***********************************************************'
+echo '*                  Rouf Automation script                 *'
+echo '*                  afrid704@gmail.com                     *'
+echo '***********************************************************'
+echo ''
 echo "1. Update
-2. Apt & snap clean
-3. Remove old snaps
-4. Update and clean
+2. Old apt & snap clean
+3. Clean old snaps only
+4. Update and clean(old apt & snap packages)
 5. Fill Slate Protocol
-6. Install nvim
-7. Remove nvim
+6. Install and configure nvim
+7. Remove nvim and its configurations
 8. Setup desktop aesthetics"
 echo ""
 echo "Enter a number"
