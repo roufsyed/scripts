4a7f8ae4f5887177b8fb6090b6678686f2d2f63a
roufsyed <afrid704@gmail.com>
2021-12-04 23:03:05 +0530

    Update

--- aesthetics.sh
+++ aesthetics.sh
@@ -34,3 +34,8 @@ gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-
 sudo curl -o /usr/share/backgrounds/catalina.jpg --create-dirs https://4kwallpapers.com/images/wallpapers/macos-catalina-mountains-island-evening-stock-5k-1366x768-4021.jpg
 gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/catalina.jpg'
 gsettings set org.gnome.desktop.background picture-options 'zoom'
+
+#favourite apps
+# ['org.gnome.Nautilus.desktop', 'brave-browser.desktop', 'torbrowser.desktop',
+# 'qbittorrent-arnatious_qbittorrent.desktop', 'android-studio_android-studio.desktop', 'nvim_nvim.desktop',
+# 'signal-desktop_signal-desktop.desktop', 'org.gnome.gitg.desktop', 'snap-store_ubuntu-software.desktop']
