4b49b2cc712d58766ecaa880d80c7445f900e1e4
roufsyed <afrid704@gmail.com>
2021-12-05 20:35:36 +0530

    Update

--- aesthetics.sh
+++ aesthetics.sh
@@ -31,9 +31,9 @@ gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color '#ffffff'
 
 # Desktop background 
-sudo curl -o /usr/share/backgrounds/catalina.jpg --create-dirs https://4kwallpapers.com/images/wallpapers/macos-catalina-mountains-island-evening-stock-5k-1366x768-4021.jpg
-gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/catalina.jpg'
-gsettings set org.gnome.desktop.background picture-options 'zoom'
+# sudo curl -o /usr/share/backgrounds/catalina.jpg --create-dirs https://4kwallpapers.com/images/wallpapers/macos-catalina-mountains-island-evening-stock-5k-1366x768-4021.jpg
+# gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/catalina.jpg'
+# gsettings set org.gnome.desktop.background picture-options 'zoom'
 
 #favourite apps
 # ['org.gnome.Nautilus.desktop', 'brave-browser.desktop', 'torbrowser.desktop',
--- fillSlate.sh
+++ fillSlate.sh
@@ -4,32 +4,40 @@ echo '*                 Rouf fillSlate Script                   *'
 echo '*                  afrid704@gmail.com                     *'
 echo '***********************************************************'
 echo ''
-sudo apt install curl
-sudo apt install zsh
-chsh -s $(which zsh)
-sudo apt install nnn
+
+sudo apt install git -y
+git config --global user.name "roufsyed"
+git config --global user.email afrid704@gmail.com
+git config --global core.editor nvim
+
+sudo apt install curl -y
+sudo apt install ripgrep -y
+sudo apt install fzf -y
+sudo apt install nnn -y
 sudo snap install android-studio --classic
 sudo snap install bpytop
-sudo snap install code --classic
 sudo snap install kotlin --classic
 sudo bash nvimInstall.sh
 sudo snap install qbittorrent-arnatious
 sudo snap install signal-desktop
 sudo snap install vlc
-sudo apt install gitg
-sudo apt install syanptic 
-sudo apt install default-jre
-sudo apt install default-jdk
+sudo apt install gitg -y
+sudo apt install syanptic -y
+sudo apt install default-jre -y
+sudo apt install default-jdk -y
 curl -sL install-node.now.sh/lts | bash
+sudo apt install dconf -y
+sudo apt install gnome-tweak-tool -y
+sudo apt install gnome-shell-extensions -y
 
-sudo apt -y install software-properties-common apt-transport-https
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo “deb [arch=amd64]  https://brave-browser-apt-release.s3.brave.com/ stable main” | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo add-apt-repository ppa:micahflee/ppa
+# From official brave website
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
+sudo apt install brave-browser
 
-sudo apt install -y brave-browser
-sudo apt install torbrowser-launcher
+sudo add-apt-repository ppa:micahflee/ppa
+sudo apt install torbrowser-launcher -y
 
-
-echo "Please Update environment variable for jdk in '/etc/environment'"
+echo "Please Update environment variable for jdk in '/etc/environment' Eg. Java_home='/usr/lib/jvm/java-11-openjdk-amd64/' "
