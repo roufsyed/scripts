7d429a8ff17f5aa58dbb8a63c4b6d285cf2c6af3
roufsyed <afrid704@gmail.com>
2021-12-05 20:35:21 +0530

    Initial commit

---
+++ nerdFontInstall.sh
@@ -0,0 +1,5 @@
+curl -fLo ~/.local/share/fonts/ubuntuMono.zip --create-dirs https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip
+cd ~/.local/share/fonts/
+unzip ubuntuMono.zip
+rm ubuntuMono.zip
+echo "UbuntuMono Nerd font is available now to set."
