7db9940bec3c8e207311bc7c52b69fe44c8c36f1
roufsyed <afrid704@gmail.com>
2021-12-07 02:39:15 +0530

    Added terminal settings

--- aesthetics.sh
+++ aesthetics.sh
@@ -6,7 +6,7 @@ gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.2
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
 gsettings set org.gnome.shell.extensions.dash-to-dock apply-custom-theme false
 
-#Alphas
+# Alphas
 gsettings set org.gnome.shell.extensions.dash-to-dock customize-alphas true
 gsettings set org.gnome.shell.extensions.dash-to-dock min-alpha 0.2
 gsettings set org.gnome.shell.extensions.dash-to-dock max-alpha 0.8
@@ -24,18 +24,42 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 34
 gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner false
 
-#Running Dot color theme
+# Running Dot color theme
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-customize-running-dots true
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-color '#ffffff'
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-border-width 0
 gsettings set org.gnome.shell.extensions.dash-to-dock custom-theme-running-dots-color '#ffffff'
 
+# Terminal settings
+gsettings set org.gnome.Terminal.Legacy.Settings tab-position bottom
+gsettings set org.gnome.Terminal.Legacy.Settings theme-variant dark
+gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ audible-bell false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ background-color 'rgb(0,0,0)'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ cursor-blink-mode off
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ cursor-shape ibeam
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ default-size-columns 98
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ default-size-rows 26
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ foreground-color 'rgb(255,255,255)'
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ use-system-font false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ use-theme-colors false
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ use-theme-transparency false
+GSETTINGS_SCHEMA=org.gnome.Terminal.Legacy.Keybindings
+GSETTINGS_PATH=/org/gnome/terminal/legacy/keybindings/
+SCHEMA_PATH=$GSETTINGS_SCHEMA:$GSETTINGS_PATH
+gsettings set $SCHEMA_PATH preferences '<ctrl><Shift>p' 
+
+# Font
+sudo bash nerdFontInstall.sh
+
 # Desktop background 
-# sudo curl -o /usr/share/backgrounds/catalina.jpg --create-dirs https://4kwallpapers.com/images/wallpapers/macos-catalina-mountains-island-evening-stock-5k-1366x768-4021.jpg
-# gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/catalina.jpg'
-# gsettings set org.gnome.desktop.background picture-options 'zoom'
+git clone https://github.com/roufsyed/wallpapers.git
+gsettings set org.gnome.desktop.background picture-uri 'file:///home/rouf/Pictures/wallpapers/mac-os-catalina-morning.jpg'
+gsettings set org.gnome.desktop.background picture-options 'zoom'
 
 #favourite apps
-# ['org.gnome.Nautilus.desktop', 'brave-browser.desktop', 'torbrowser.desktop',
-# 'qbittorrent-arnatious_qbittorrent.desktop', 'android-studio_android-studio.desktop', 'nvim_nvim.desktop',
-# 'signal-desktop_signal-desktop.desktop', 'org.gnome.gitg.desktop', 'snap-store_ubuntu-software.desktop']
+# ['org.gnome.Nautilus.desktop', 'brave-browser.desktop', 'signal-desktop_signal-desktop.desktop', 'qbittorrent-arnatious_qbittorrent.desktop', 'android-studio_android-studio.desktop', 'ca.desrt.dconf-editor.desktop', 'nvim_nvim.desktop', 'org.gnome.gitg.desktop', 'snap-store_ubuntu-software.desktop']
+
+#enabled-extensions
+# ['ubuntu-appindicators@ubuntu.com', 'hidetopbar@mathieu.bidon.ca', 'simplenetspeed@biji.extension', 'horizontal-workspaces@gnome-shell-extensions.gcampax.github.com', 'windowsNavigator@gnome-shell-extensions.gcampax.github.com', 'hide-activities-button@nmingori.gnome-shell-extensions.org', 'transparent-top-bar@ftpix.com', 'ubuntu-dock@ubuntu.com']
