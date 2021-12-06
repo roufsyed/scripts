7b28c2c72f7df1fdfea7e6dc2ba124db399be4b1
roufsyed <afrid704@gmail.com>
2021-12-07 02:40:04 +0530

    Added command to set font

--- nerdFontInstall.sh
+++ nerdFontInstall.sh
@@ -2,4 +2,4 @@ curl -fLo ~/.local/share/fonts/ubuntuMono.zip --create-dirs https://github.com/r
 cd ~/.local/share/fonts/
 unzip ubuntuMono.zip
 rm ubuntuMono.zip
-echo "UbuntuMono Nerd font is available now to set."
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ font 'UbuntuMono Nerd Font Mono 12'
