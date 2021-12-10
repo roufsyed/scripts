bdec8af43c46513103a9592b9901700dd10449c6
roufsyed <afrid704@gmail.com>
2021-12-10 12:53:44 +0530

    Initial commit

---
+++ dotFilesInstall.sh
@@ -0,0 +1,4 @@
+git clone git@github.com:roufsyed/dotFiles.git ~/Github\ Repositories/dotFiles/
+ln ~/Github\ Repositories/dotFiles/.bashrc ~/.bashrc
+ln ~/Github\ Repositories/dotFiles/.inputrc ~/.inputrc
+ln ~/Github\ Repositories/dotFiles/.gitconfig ~/.gitconfig
