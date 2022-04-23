4f71fad0801479e12ddaeadb6afc2c35f27adb0c
roufsyed <afrid704@gmail.com>
2022-04-23 13:41:05 +0530

    Updated https link to the repo

--- dotFilesInstall.sh
+++ dotFilesInstall.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:roufsyed/dotFiles.git ~/Github\ Repositories/dotFiles/
+git clone https://github.com/roufsyed/dotFiles.git ~/Github\ Repositories/dotFiles/
 ln ~/Github\ Repositories/dotFiles/.bashrc ~/.bashrc
 ln ~/Github\ Repositories/dotFiles/.inputrc ~/.inputrc
 ln ~/Github\ Repositories/dotFiles/.gitconfig ~/.gitconfig
