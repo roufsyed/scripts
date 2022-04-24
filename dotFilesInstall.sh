63f89aa58959b40179b4d34aeead74af67e16a66
roufsyed <afrid704@gmail.com>
2022-04-25 00:51:04 +0530

    Added a command to run install script from dotFiles repo

--- dotFilesInstall.sh
+++ dotFilesInstall.sh
@@ -1,4 +1,14 @@
 git clone https://github.com/roufsyed/dotFiles.git ~/Github\ Repositories/dotFiles/
-ln ~/Github\ Repositories/dotFiles/.bashrc ~/.bashrc
-ln ~/Github\ Repositories/dotFiles/.inputrc ~/.inputrc
-ln ~/Github\ Repositories/dotFiles/.gitconfig ~/.gitconfig
+bash ~/Github Repositories/dotFiles/install.sh
+
+## bashrc
+#ln ~/Github\ Repositories/dotFiles/.bashrc ~/.bashrc
+
+##.inputrc to make terminal cursor change according to normal and insert mode
+#ln ~/Github\ Repositories/dotFiles/.inputrc ~/.inputrc
+
+## .gitconfig
+#ln ~/Github\ Repositories/dotFiles/.gitconfig ~/.gitconfig
+
+## tmux config
+#ln ~/Github\ Repositories/dotFiles/.tmux.config ~/.tmux.conf
