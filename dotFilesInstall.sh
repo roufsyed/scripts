5ab9fa946de4599099832852a15327ede4b8fc01
roufsyed <afrid704@gmail.com>
2022-04-28 02:55:15 +0530

    Refactored

--- dotFilesInstall.sh
+++ dotFilesInstall.sh
@@ -1,14 +1,4 @@
-git clone https://github.com/roufsyed/dotFiles.git ~/Github\ Repositories/dotFiles/
-bash ~/Github Repositories/dotFiles/install.sh
-
-## bashrc
-#ln ~/Github\ Repositories/dotFiles/.bashrc ~/.bashrc
-
-##.inputrc to make terminal cursor change according to normal and insert mode
-#ln ~/Github\ Repositories/dotFiles/.inputrc ~/.inputrc
-
-## .gitconfig
-#ln ~/Github\ Repositories/dotFiles/.gitconfig ~/.gitconfig
-
-## tmux config
-#ln ~/Github\ Repositories/dotFiles/.tmux.config ~/.tmux.conf
+git clone https://github.com/roufsyed/dotFiles.git ~/
+cd ~/dotFiles/
+bash install.sh
+cd ~/scripts/
