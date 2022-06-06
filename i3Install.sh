0c2082b5c210ee4a70729c0d2eb3d3282f999ca5
roufsyed <afrid704@gmail.com>
2022-06-06 13:59:14 +0530

    Added touchpad touch config for ubuntu x11 systems

--- i3Install.sh
+++ i3Install.sh
@@ -1,6 +1,22 @@
 sudo apt install i3 -y
-sudo apt install arandr -y
 sudo apt install feh -y
+sudo apt install arandr -y
 sudo apt install lxappearance -y
+
+# touchpad tap configuration for ubuntu x11
+sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+Section "InputClass"
+        Identifier "touchpad"
+        MatchIsTouchpad "on"
+        Driver "libinput"
+        Option "Tapping" "on"
+        Option "TappingButtonMap" "lrm"
+        Option "NaturalScrolling" "on"
+        Option "ScrollMethod" "twofinger"
+EndSection
+
+EOF
+
+# playerctl not installed by default on ubuntu and is not available on repositories, so have to download manually.
 
 # TODO : back all gtk files and touchpad configuration x11
